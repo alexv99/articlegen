ab8158b0625294afe6d0e29805b0546664e0a535
alexv99 <alex@vauthey.com>
2023-11-27 18:11:58 -0800

    change model

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,6 @@ from langchain.llms import GPT
 from langchain.chains import SingleModelChain
 
 st.title('Quickstart App')
-openai_api_key = "st.sidebar.text_input('OpenAI API Key')"
 
 def generate_response(input_text):
     llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key, model_name="gpt-4")
@@ -27,7 +26,5 @@ def generate_response(input_text):
 with st.form('my_form'):
   text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
   submitted = st.form_submit_button('Submit')
-  if not openai_api_key.startswith('sk-'):
-    st.warning('Please enter your OpenAI API key!', icon='⚠')
-  if submitted and openai_api_key.startswith('sk-'):
+  if submitted:
     generate_response(text)
