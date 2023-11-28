1f180f799ecddde5048fe8ba0065b0a0e2c2de25
alexv99 <alex@vauthey.com>
2023-11-27 17:15:43 -0800

    add requirements

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,11 @@
 import streamlit as st
-from langchain.llms import openai
+from langchain.llms import OpenAI
 
 st.title('Quickstart App')
 openai_api_key = st.sidebar.text_input('OpenAI API Key')
 
 def generate_response(input_text):
-  llm = openai(temperature=0.7, openai_api_key=openai_api_key)
+  llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
   st.info(llm(input_text))
 
 with st.form('my_form'):
