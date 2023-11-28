b56fcccdee1da69627339823bb0cd3356dbd3c3b
alexv99 <alex@vauthey.com>
2023-11-27 17:28:53 -0800

    change model

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ st.title('Quickstart App')
 openai_api_key = st.sidebar.text_input('OpenAI API Key')
 
 def generate_response(input_text):
-  llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key, model_name="gpt4-32k")
+  llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key, model_name="gpt-4")
   st.info(llm(input_text))
 
 with st.form('my_form'):
